1a76dc8e382b2b6624e8aa345e7fa1eea793369f
Eugen Ryabinin <eugene.ryabinin@devexpress.com>
2020-01-14 15:40:53 +0300

    Updates

---
+++ precache-manifest.11a51e3d4088bdb2dde615a5c0f4fb67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7574030568a44bad1ae9000c76551325",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "f950845a05ff8836cdd9",
+    "url": "/TodoList/static/css/2.baf5c7fd.chunk.css"
+  },
+  {
+    "revision": "75e5fd757598a6c697d2",
+    "url": "/TodoList/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f950845a05ff8836cdd9",
+    "url": "/TodoList/static/js/2.34c35bcd.chunk.js"
+  },
+  {
+    "revision": "75e5fd757598a6c697d2",
+    "url": "/TodoList/static/js/main.1987b443.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  }
+]);
